04a86fc9eaa28d6acbd5fb329cd7829d20886183
Buga Eudochia <docicapitu@gmail.com>
2026-03-17 22:54:32 +0200

    Add files via upload

--- SistemeAlerta.cs
+++ SistemeAlerta.cs
@@ -8,46 +8,49 @@ namespace Monitorizare_Sanatate
 {
     internal class SistemeAlerta
     {
-        public void VerificareGlicemie(MasuratoriGlicemie masuratori)
+        public string VerificareGlicemie(MasuratoriGlicemie masuratori)
         {
             if (masuratori.ValoareGlicemie<70)
             {
-                Console.WriteLine($"Alerta! Glicemie scazuta pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Valoare:{masuratori.ValoareGlicemie} {masuratori.UnitateMasura}");
+                return $"Alerta! Glicemie scazuta pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Valoare:{masuratori.ValoareGlicemie} {masuratori.UnitateMasura}";
 
             }
             else if (masuratori.ValoareGlicemie>180)
             {
-                Console.WriteLine($"Alerta! Glicemie crescuta pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Valoare: {masuratori.ValoareGlicemie} {masuratori.UnitateMasura}");
+                return $"Alerta! Glicemie crescuta pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Valoare: {masuratori.ValoareGlicemie} {masuratori.UnitateMasura}";
 
             }
+            return $"Glicemie normala pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Valoare:{masuratori.ValoareGlicemie} {masuratori.UnitateMasura}";
         }
 
-        public void VerificareTensiune(MasuratoriCardiovasculare masuratori)
+        public string VerificareTensiune(MasuratoriCardiovasculare masuratori)
         {
             if (masuratori.TensiuneaSistolica>140 || masuratori.TensiuneaDiastolica>90 )
             {
-                Console.WriteLine($"Alerta! Tensiune arteriala crescuta pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Valoare: {masuratori.TensiuneaSistolica}/{masuratori.TensiuneaDiastolica} mmHg");
+                return $"Alerta! Tensiune arteriala crescuta pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Valoare: {masuratori.TensiuneaSistolica}/{masuratori.TensiuneaDiastolica} mmHg";
             }
             else if (masuratori.TensiuneaSistolica<90 || masuratori.TensiuneaDiastolica<60)
             {
-                Console.WriteLine($"Alerta!Tensiune arteriala scazuta pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Valoare: {masuratori.TensiuneaSistolica}/{masuratori.TensiuneaDiastolica} mmHg");
+                return $"Alerta!Tensiune arteriala scazuta pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Valoare: {masuratori.TensiuneaSistolica}/{masuratori.TensiuneaDiastolica} mmHg";
 
             }
+            return $"Tensiune arteriala normala pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Vaaloare:{masuratori.TensiuneaSistolica}/{masuratori.TensiuneaDiastolica} mmHg";
         }
 
 
-        public void VerificarePuls(MasuratoriCardiovasculare masuratori)
+        public string VerificarePuls(MasuratoriCardiovasculare masuratori)
         {
             if(masuratori.Puls>100)
             {
-                Console.WriteLine($"Alerta!Puls crescut pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Valoare:{masuratori.Puls}bpm");
+                return $"Alerta!Puls crescut pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Valoare:{masuratori.Puls}bpm";
 
 
             }
             else if(masuratori.Puls<60)
             {
-                Console.WriteLine($"Alerta!Puls scazut pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Valoare:{masuratori.Puls}bpm");
+                return $"Alerta!Puls scazut pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Valoare:{masuratori.Puls}bpm";
             }
+            return $"Puls normal pentru pacientul {masuratori.Pacient.Nume} {masuratori.Pacient.Prenume}. Valoare:{masuratori.Puls}bpm";
         }
         
     }
